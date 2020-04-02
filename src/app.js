0d09e0a2cf5358bb88efc6e24d90a6de1cc848aa
Nicholas Alves <nicholas.alves14@gmail.com>
2020-04-02 13:10:00 -0300

    Allow cors

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const { errors } = require('celebrate');
+require('dotenv/config');
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.WEB_BTH,
-}));
+const whitelist = [process.env.WEB_BTH, process.env.LOCALHOST];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(routes);
