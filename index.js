153e2a994f364f216f775b0f292d6dab81b34e44
Nicholas Alves <nicholas.alves14@gmail.com>
2020-04-01 22:07:55 -0300

    Fix npm scripts

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Worldo');
+    res.send('O PROBLEMA ERA O START');
 });
 
 app.listen(process.env.PORT || 3333);
