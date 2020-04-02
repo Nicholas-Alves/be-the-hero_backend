3393338438b6d9993b75722789d6b547a6919909
Nicholas Alves <nicholas.alves14@gmail.com>
2020-04-01 23:25:49 -0300

    s/ cors

--- src/app.js
+++ src/app.js
@@ -1,20 +1,15 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const routes = require('./routes');
 const { errors } = require('celebrate');
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
-/**
- * HOST:
- * 
- * app.use(cors({
- *  origin: '<endereço da hospedagem>'
- * }));
- */
-
+// app.use(cors({
+//     origin: process.env.WEB_BTH,
+// }));
 
 app.use(express.json());
 app.use(routes);
